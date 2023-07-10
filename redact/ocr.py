72d98b2e66b660f6299b0ac87d373975a34e1bbc
Raja Raju <pgrajaraju@hotmail.com>
2023-07-11 00:11:52 +0530

    post processing ocr text

--- redact/ocr.py
+++ redact/ocr.py
@@ -6,10 +6,23 @@ import pandas as pd
 image_path = "C:\\Users\\RAJARAJU\\Downloads\\housing.png"
 text_info = pytesseract.image_to_data(Image.open(image_path))
 
-print(type(text_info))
-all_data = StringIO(text_info)
+# print(text_info.split('\n')[0].split('\t'))
+# print(text_info.split('\n')[0].split('\t')[6:10:12])
 
-df = pd.read_csv(all_data)
-print(df)
+text_bounding_boxes = {}
+# with open("ocr_bb.json", 'w+') as fw:
+for single_line in text_info.split('\n'):
+    print(f"line split: {single_line}")
+    ents = single_line.split('\t')
+    print(f"ENTS: {ents}")
+    text_bounding_boxes.update({ents[-1] :
+                                {
+                                    'x' : ents[6],
+                                    'y' : ents[7],
+                                    'width' : ents[8],
+                                    'height' : ents[9]
+                                }})
+    
+print(text_bounding_boxes)
 
-df.to_csv("ocr_data.csv")
+
