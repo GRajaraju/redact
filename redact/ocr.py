09bcdd638fa57e06e7e26ab25f80fb5b919618a1
Raja Raju <pgrajaraju@hotmail.com>
2023-07-11 22:44:15 +0530

    removed commented code

--- redact/ocr.py
+++ redact/ocr.py
@@ -6,11 +6,7 @@ import pandas as pd
 image_path = "C:\\Users\\RAJARAJU\\Downloads\\housing.png"
 text_info = pytesseract.image_to_data(Image.open(image_path))
 
-# print(text_info.split('\n')[0].split('\t'))
-# print(text_info.split('\n')[0].split('\t')[6:10:12])
-
 text_bounding_boxes = {}
-# with open("ocr_bb.json", 'w+') as fw:
 for single_line in text_info.split('\n'):
     print(f"line split: {single_line}")
     ents = single_line.split('\t')
